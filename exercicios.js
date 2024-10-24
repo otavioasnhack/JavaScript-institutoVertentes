b430ec2dce3f554181e9f52915a9e54f596b1894
otavioasnhack <otavioasnhack@gmail.com>
2024-10-24 06:52:03 -0300

    23/10/24 primeira aula

---
+++ exercicios.js
@@ -0,0 +1,73 @@
+// Função que recebe lista de nums e retorna o maior deles
+
+// function ReturnNums(list) {
+//     // return Math.max(...nums)
+//    let nums = list[0]
+//    for (let index = 1; index < list.length; index++) {
+//     if (list[index] > nums) {
+//         nums = list[index]
+//     }
+//    }
+//    return nums
+// }
+// console.log(ReturnNums([1, 2, 3]));
+// console.log(ReturnNums([4, 20, 3]));
+// console.log(ReturnNums([12, 8, 39]));
+
+
+// -----------------------------------------------------------------------------------------------------------------
+
+
+// Função que recebe uma lista de números e retorna só os números pares
+
+// function pares(nums) {
+//     let numsPares = []
+//     for (let index = 0; index < nums.length; index++) {
+//         if (nums[index] % 2 === 0) {
+//             numsPares.push(nums[index])
+//         }
+//     }
+//     return numsPares
+// }
+// console.log(pares([12, 8, 39, 3, 5, 48, 56, 57]));
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Função que recebe uma lista de número e um outro número e retorne todos os números multiplicados pelo número passado por parâmetro
+
+// function mult(list, num) {
+//     let newList = []
+//     for (let index = 0; index < list.length; index++) {
+//         newList.push(list[index] * num)
+//     }
+//     return newList
+// }
+
+//  console.log(mult([1, 2, 3], 2));
+//  console.log(mult([4, 20, 3], 3));
+
+
+// -----------------------------------------------------------------------------------------------------------------
+
+// Função que recebe uma lista de palavras e retorna quantas vezes cada palavra se repete
+
+// function words(list) {
+//     let numWord = {};
+
+//     for (let index = 0; index < list.length; index++) {
+//         let word = list[index];
+
+//         if (numWord[word]) {
+//             numWord[word]++;
+//         } else {
+//             numWord[word] = 1;
+//         }
+//         } 
+//         return numWord;
+//     }
+//     console.log(words(['banana', 'pera', 'maçã', 'banana', 'banana', 'pera', 'maçã', 'banana', 'uva']));
+
+
+ 
+  
+    
